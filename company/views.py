f693c70ef653845336d41b7dd99105070678dc59
balqeesmaaytah <balqeesmaaytah5@gmail.com>
2025-04-02 19:53:43 +0300

    Update views.py

--- company/views.py
+++ company/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
+from account.forms import CompanyVerificationForm
+from company.form import CompanyProfileForm
+def company_profile(request):
+    company = get_object_or_404(company, id=1)
+    return render(request, 'inf.html', {'company': company})
 
-# Create your views here.
+def company_create(request):
+    if request.method == "POST":
+        form = CompanyVerificationForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('company_profile')
+    else:
+        form = CompanyProfileForm()
+    
+    return render(request, 'add.html', {'form': form, 'action': 'Create'})
+
+def company_update(request, company_id):
+    company = get_object_or_404(company, id=company_id)
+    if request.method == "POST":
+        form = CompanyProfileForm(request.POST, request.FILES, instance=company)
+        if form.is_valid():
+            form.save()
+            return redirect('company_profile')
+    else:
+        form =  CompanyProfileForm(instance=company)
+    
+    return render(request, 'add.html', {'form': form, 'action':'Update'})
