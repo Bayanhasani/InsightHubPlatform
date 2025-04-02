c063d8ccf51d2be1604996d25977483f53a9875e
Bayanhasani <117782909+Bayanhasani@users.noreply.github.com>
2025-04-02 16:54:35 +0300

    Update url.py

--- company/url.py
+++ company/url.py
@@ -5,7 +5,3 @@ urlpatterns = [
     path('companies/new/', company_create, name='company_create'),
     path('companies/<int:pk>/edit/', company_update, name='company_update'),
 ]
-#يسمح برفع صور الشعاراات وحغظها في مجلد اسمه media
-from django.conf import settings
-from django.conf.urls.static import static
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
