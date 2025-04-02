5d6c9955d32ffd4bdcbaf9b1137e01226f26db50
Bayanhasani <117782909+Bayanhasani@users.noreply.github.com>
2025-04-02 19:34:01 +0300

    Update form.py

--- company/form.py
+++ company/form.py
@@ -1,6 +1,1 @@
-from django import forms
-from .models import CompanyProfile
-class CompanyProfileForm(forms.ModelForm):
-    class Meta:
-        model = CompanyProfile
-        fields = ['name', 'description', 'email', 'phone', 'address', 'website','logo']
+
