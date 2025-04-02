8980949fcef906e903eb44b345258e3b411f999c
balqeesmaaytah <balqeesmaaytah5@gmail.com>
2025-04-02 19:52:23 +0300

    Update models.py

--- company/models.py
+++ company/models.py
@@ -1,14 +1,15 @@
 from django.db import models
 from account import models
-# Create your models here.
-class CompanyProfile(models.Model):
-    name = models.CharField(max_length=255, verbose_name="اسم الشركة")
-    description = models.TextField(blank=True, null=True, verbose_name="وصف الشركة")
-    email = models.EmailField(unique=True, verbose_name="البريد الإلكتروني")
-    phone = models.CharField(max_length=20, verbose_name="رقم الهاتف")
-    address = models.CharField(max_length=255, verbose_name="العنوان")
-    website = models.URLField(blank=True, null=True, verbose_name="الموقع الإلكتروني")
-    logo = models.ImageField(upload_to='company_logos/', blank=True, null=True, verbose_name="شعار الشركة")
-
+from django.db import models
+from account.models import CustomUser  # استيراد CustomUser
+class Customer(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, related_name="customer_profile")
+    company_name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=20, unique=True)
+    address = models.CharField(max_length=255, blank=True, null=True)
+    website = models.URLField(blank=True, null=True)
+    logo = models.ImageField(upload_to="company_logos/", blank=True, null=True)
     def _str_(self):
-        return self.name 
+        return self.company_name
